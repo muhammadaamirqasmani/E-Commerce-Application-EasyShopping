62cd412981bae7868ed69b5cfdbcc5f3c874c84d
muhammadaamirqasmani <30751626+muhammadaamirqasmani@users.noreply.github.com>
2017-12-02 18:38:27 +0500

    add Login Screen and Signup Screen

---
+++ SHOPSTA/SHOPSTA/ViewController/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SHOPSTA
+//
+//  Created by Techsviewer on 12/2/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
