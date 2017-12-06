b22f1f1cb250573f90ccb626ded16247c4626d74
MAQ <muhammadaamirqasmani@gmail.com>
2017-12-05 23:56:24 -0800

    some work on collection view

--- SHOPSTA/SHOPSTA/ViewController/HomeVC.swift
+++ SHOPSTA/SHOPSTA/ViewController/HomeVC.swift
@@ -12,25 +12,31 @@ import ObjectMapper
 import SDWebImage
 
 
-class HomeVC: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
+class HomeVC: UIViewController {
  
 
     @IBOutlet weak var pictureCollection: UICollectionView!
     
+    var picturearray = [pictureObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pictureCollection.delegate = self
-        pictureCollection.dataSource = self
+//        pictureCollection.delegate = self
+//        pictureCollection.dataSource = self
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return picturearray.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCollectionVC
+//
+//        cell.ItemImage.sd_setImage(with: URL(string: picturearray[indexPath.row].ImageURL!), placeholderImage: UIImage(named: "placeholder.png"))
+//        return cell
+//
+//    }
     
 
 }
